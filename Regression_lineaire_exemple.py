d21d02640d6345bf423c5104a57ae9481078a293
admin-poste <aouragh.faissal@gmail.com>
2023-10-25 01:26:45 +0100

    1fst commit

--- Regression_lineaire_exemple.py
+++ Regression_lineaire_exemple.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 
- 
+ #1fst commit
 data_url = "http://lib.stat.cmu.edu/datasets/boston"
 raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
 data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
